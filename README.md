e08fcd6d9416cbe4e92494a048112d54d97631aa
Brenda Jin <info@brendajin.com>
2014-11-13 23:35:38 -0800

    created automatic generation of zip file, updated links, and added README.md

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 jasmine-backbone
 ================
 
-Jasmine Testing for Backbone.js Models and Views
+#Jasmine Testing for Backbone.js Models and Views
+
+Behavior-driven development with Jasmine testing enables developers to create thoughtful software design. Testing helps us match code to expectation, and communicates our intentions to other developers. When coupled with Backbone.js, Jasmine can become a powerful tool in ensuring the scalability of our web application. In addition, testing on Views in the browser helps us find bugs before the effort of visual QA.
+
+This session will cover a breakdown of how to approach Jasmine testing for Backbone.js Models and Views, how Jasmine testing works with instantiating new Models and Views, and how to trigger and test custom events and their callbacks.
--- web.js
+++ web.js
@@ -1,11 +1,27 @@
 var express 		= require('express'),
-app 				= express();
+	app 			= express(),
+	fs				= require('fs'), 
+	archiver		= require('archiver'),
+	exampleName 	= 'jasmine-backbone-example.zip',
+	output 			= fs.createWriteStream(exampleName),
+	zip				= archiver('zip');
 
 //function for heroku
 process.env.PWD = process.cwd();
 
+zip.pipe(output);
 
-app.configure(function(){
+zip.bulk([
+	{ expand: true, src: ['example/*'], cwd: 'public/', dest: '.' }
+]);
+
+zip.finalize();
+
+app.get('/example', function( req, res ) {
+	res.download( __dirname + '/' + exampleName );
+});
+
+app.configure( function () {
 	//public directory for static server
 	app.use(express.static(__dirname + '/public'));
 	//ignore favicon
@@ -13,6 +29,6 @@ app.configure(function(){
 });
 
 var port = process.env.PORT || 2500;
-app.listen(port, function() {
+app.listen( port, function () {
   console.log("LISTENING ON PORT: "+port);
 });
