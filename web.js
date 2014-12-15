1523eebb9ff8d6130cdc1010ed920ce92bf87234
Brenda Jin <brendajin@gmail.com>
2014-12-15 03:40:04 -0800

    removed zipping due to recursive error in walking through the files, removed favicon error

--- web.js
+++ web.js
@@ -1,21 +1,17 @@
 var express 		= require('express'),
 	app 			= express(),
-	fs				= require('fs'), 
-	archiver		= require('archiver'),
-	exampleName 	= 'example.zip',
-	output 			= fs.createWriteStream(exampleName),
-	zip				= archiver('zip');
+	fs				= require('fs');
 
 //function for heroku
 process.env.PWD = process.cwd();
 
-zip.pipe(output);
+// zip.pipe(output);
 
-zip.bulk([
-	{ expand: true, src: ['example/*'], cwd: 'public/', dest: '.' }
-]);
+// zip.bulk([
+// 	{ expand: true, src: ['example/*'], cwd: 'public/', dest: '.' }
+// ]);
 
-zip.finalize();
+// zip.finalize();
 
 app.get('/example', function( req, res ) {
 	res.download( __dirname + '/' + exampleName );
@@ -24,8 +20,6 @@ app.get('/example', function( req, res ) {
 app.configure( function () {
 	//public directory for static server
 	app.use(express.static(__dirname + '/public'));
-	//ignore favicon
-	app.use(express.favicon(process.env.PWD + '/public/favicon.ico'));
 });
 
 var port = process.env.PORT || 2500;
